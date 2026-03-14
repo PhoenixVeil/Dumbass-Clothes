5a2a67cdfd5a6ebfd3b911091997c57fb68e7e94
Phoenix <phoenixveilrp@gmail.com>
2026-03-14 15:58:38 -0700

    3

---
+++ [clothes]/rcore_addon_tattoos_all_preset/fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Isigar <info@rcore.cz>'
+description 'Addon tattoos generated with addon-tattoos.com tool'
+discord 'https://discord.com/invite/F28PfsY'
+tool_path 'addon-tattoos.com'
+version '1.1.0'
+
+files {
+     'rcore_overlays.xml',
+     'rcore_overlays_action-anime-pack.xml',
+     'rcore_overlays_army-tattoo.xml',
+     'rcore_overlays_faith.xml',
+     'rcore_overlays_geishas-and-dragons.xml',
+     'rcore_overlays_mafia-kings.xml',
+     'rcore_overlays_oldschool.xml',
+     'rcore_overlays_rm.xml',
+     'rcore_overlays_sons-of-anarchy.xml',
+     'rrcore_overlays_animals.xml',
+     'rcore_overlays_valentine.xml',
+     'rcore_overlays_viking-tattoo.xml', 
+     'rcore_overlays_gangster.xml',
+   
+}
+
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_action-anime-pack.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_animals.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_army-tattoo.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_faith.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_geishas-and-dragons.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_mafia-kings.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_oldschool.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_rm.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_valentine.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_viking-tattoo.xml'
+data_file 'PED_OVERLAY_FILE' 'rcore_overlays_gangster.xml'
+
+
+dependency '/assetpacks'
